6eeff08fe2fa88397dbcad00fc600b5c5a0631d5
John Hosie <jhosie@uk.ibm.com>
2018-08-20 18:39:32 +0100

    quotes

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 set -x
 export INTEGRATION_DEPLOYMENT_NAME=copy-leads-to-campaign
 export NEW_INTEGRATION_DEPLOYMENT_FILE=${TRAVIS_BUILD_DIR}/newDeployment.json
-export INTEGRATION_YAML_FILE=${TRAVIS_BUILD_DIR}/Copy\ Leads\ to\ Campaign.yaml
+export INTEGRATION_YAML_FILE="${TRAVIS_BUILD_DIR}/Copy Leads to Campaign.yaml"
 pwd
 echo $TRAVIS_BUILD_DIR
 
