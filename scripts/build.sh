cf360ca5a0f5579a61122586210efe6666f57cfe
John Hosie <jhosie@uk.ibm.com>
2018-08-20 18:47:39 +0100

    other quotes

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,10 @@
 set -x
 export INTEGRATION_DEPLOYMENT_NAME=copy-leads-to-campaign
 export NEW_INTEGRATION_DEPLOYMENT_FILE=${TRAVIS_BUILD_DIR}/newDeployment.json
-export INTEGRATION_YAML_FILE="${TRAVIS_BUILD_DIR}/Copy Leads to Campaign.yaml"
+export INTEGRATION_YAML_FILE=${TRAVIS_BUILD_DIR}/Copy Leads to Campaign.yaml
 pwd
 echo $TRAVIS_BUILD_DIR
 
 npm install
 
-node ${TRAVIS_BUILD_DIR}/scripts/build.js ${INTEGRATION_YAML_FILE} ${INTEGRATION_DEPLOYMENT_NAME}
+node ${TRAVIS_BUILD_DIR}/scripts/build.js "${INTEGRATION_YAML_FILE}" ${INTEGRATION_DEPLOYMENT_NAME}
