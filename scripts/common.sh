fedfb3978430a0384d8c208f1cdf573e65eab282
John Hosie <jhosie@uk.ibm.com>
2018-08-28 21:36:23 +0100

    use account 1 and rename back to long version

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,3 +1,3 @@
-export FLOW_NAME="Populate a Salesforce campaign"
+export FLOW_NAME="Populate a Salesforce campaign with leads captured in IBM Db2"
 export NEW_INTEGRATION_DEPLOYMENT_FILE=${TRAVIS_BUILD_DIR}/build-output.json
 export INTEGRATION_DEPLOYMENT_NAME=$( echo  ${FLOW_NAME} | sed "s/ /-/g" )
